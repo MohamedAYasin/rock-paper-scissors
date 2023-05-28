16292335e1a03c3e5f2dbf2a34e217f0d96fbf5c
Your Name <mayasinalu@gmail.com>
2023-05-28 17:09:57 +0300

    rock paper scissors

--- challenge.js
+++ challenge.js
@@ -1,2 +1,18 @@
 // Good Luck! You got this 💪🏾
-// Write your code here.
+// creating a classic rock, paper and scissors game while determining the winner.
+
+
+function playGame(play1, play2) {
+    if (play1 === play2) {
+        return "tie";
+    } else if (
+        (play1 === "rock" && play2 === "scissors") || (play1 === "paper" && play2 === "rock") || (play1 === "scissors" && play2 === "paper")
+    ) {
+        return "Player One win";
+    } else {
+        return "Player Two win";
+    }
+}
+console.log(playGame("paper", "scissors"));
+console.log(playGame("rock", "paper"));
+console.log(playGame("scissors", "scissors"));
